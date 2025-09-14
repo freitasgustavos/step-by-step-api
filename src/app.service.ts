20ea86231dcee6dfbcc03b22be681e4fd3d537a7
Gustavo Freitas <freitasgustavos@hotmail.com>
2025-09-14 11:58:30 -0300

    info api

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,14 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello(): object {
+    return {
+      name: 'setp-by-step-api',
+      version: '0.0.1',
+      environment: process.env.NODE_ENV || 'development',
+      routes: ['/cep/:cep', '/submission'],
+      timestamp: new Date().toISOString(),
+      message: 'Bem-vindo à Step-by-Step API!',
+    };
   }
 }

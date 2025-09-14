a83c0b501aa4ce3f4e0d2f8561f573edf3b01b9c
Gustavo Freitas <freitasgustavos@hotmail.com>
2025-09-14 13:07:49 -0300

    Atualizando teste app.controller

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,12 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return API info object', () => {
+      const result = appController.getHello();
+      expect(result).toHaveProperty('name');
+      expect(result).toHaveProperty('version');
+      expect(result).toHaveProperty('routes');
+      expect(result).toHaveProperty('timestamp');
     });
   });
 });

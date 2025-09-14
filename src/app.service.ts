5b170ada682495dec2e1f7fd33d2b23c302cea81
Gustavo Freitas <freitasgustavos@hotmail.com>
2025-09-14 12:53:13 -0300

    Removido o environment

--- src/app.service.ts
+++ src/app.service.ts
@@ -6,7 +6,6 @@ export class AppService {
     return {
       name: 'setp-by-step-api',
       version: '0.0.1',
-      environment: process.env.NODE_ENV || 'development',
       routes: ['/cep/:cep', '/submission'],
       timestamp: new Date().toISOString(),
       message: 'Bem-vindo à Step-by-Step API!',
